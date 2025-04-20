b4e43b5bf0a9b94ab6da0de4c1ecf61ea9ffb8c8
Mandla Jiane <55124856+jman3000c@users.noreply.github.com>
2025-04-20 16:36:04 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,4 +1,4 @@
 --  an SQL query to create a new database called salesDB
-CREATE DATABASE IF NOT EXISTS SalesDB;
+CREATE DATABASE SalesDB;
 -- an SQL query to drop (delete) the database called demo
-DROP DATABASE IF EXISTS demo;
+DROP DATABASE demo;
